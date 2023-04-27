c177c236cc130904217fe37b4f037d1f82563f42
Heather Greenberg <hgreenberg1017@gmail.com>
2023-04-26 23:46:25 -0700

    add webpack configs

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,9 +2,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const { default: EditorPrompt } = require('inquirer/lib/prompts/editor');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+// Add and configure workbox plugins for a service worker and manifest file.
+
+//Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -18,11 +20,50 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: TextEditor
+      }),
+      new InjectManifest(
+        {
+          swDrc: './src-sw.js',
+          swSrc: './src-sw.js',
+        }),
+        new WebpackPwaManifest({
+          fingerprints: false,
+          inject: true,
+          name: TextEditor,
+          short_name: editor,
+          description: 'An offline text editor application',
+          background_color: '#ffffff',
+          theme_color: '#ffffff',
+          start_url: '/',
+          publicPath: '/',
+          icons: [
+            { 
+              src: path.resolve('src/images/icon-192x192.png'),
+              sizes: [96, 128, 192, 256, 384, 512],
+              destination: path.join('assets', 'icons'),
+            },
     ],
 
     module: {
       rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread'],
+            },
+          },
+        }
         
       ],
     },
